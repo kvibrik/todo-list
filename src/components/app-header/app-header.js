63972d6961e01b6e3703549468a5eb43ebdbc898
kvibrik <kvibr@mail.ru>
2021-02-18 21:40:10 +0500

    add AppHeader component

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app-header.scss';
+
+const AppHeader = () => {
+  return (
+    <div className="app-header">
+      <h1>Todo List</h1>
+      <h2>1 more to do, 2 done</h2>
+    </div>
+  );
+};
+
+export default AppHeader;
