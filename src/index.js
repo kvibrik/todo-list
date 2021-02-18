c44a490dfb0b19ad9e767aa1855c7497ae3b3596
kvibrik <kvibr@mail.ru>
2021-02-18 21:19:13 +0500

    добавил reset.scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import './assets/scss/reset.scss';
 import App from './components/app';
 
 ReactDOM.render(<App />, document.getElementById('root'));
