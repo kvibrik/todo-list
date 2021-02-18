e52b5f033ef7f47b085aec8fff5eff37064f0de6
kvibrik <kvibr@mail.ru>
2021-02-18 21:46:29 +0500

    styles for app component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+// Components
+import AppHeader from '../app-header';
 
 import './App.scss';
 
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div></div>
+      <div className="app">
+        <AppHeader />
       </div>
     );
   }
