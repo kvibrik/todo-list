9ce0b426408928aa2b9ebdd872b1e6d485735b44
kvibrik <kvibr@mail.ru>
2021-02-18 21:54:55 +0500

    app-header styles

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -4,8 +4,8 @@ import './app-header.scss';
 const AppHeader = () => {
   return (
     <div className="app-header">
-      <h1>Todo List</h1>
-      <h2>1 more to do, 2 done</h2>
+      <h1 className="app-header__title">Todo List</h1>
+      <h2 className="app-header__subtitle">1 more to do, 2 done</h2>
     </div>
   );
 };
