f5732701739623438e7c81c5091359f3b5cb8639
kvibrik <kvibr@mail.ru>
2021-02-18 21:34:48 +0500

    поменял стили

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,22 +1,13 @@
+import React, { Component } from 'react';
+
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div></div>
+      </div>
+    );
+  }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import './assets/scss/reset.scss';
+import './index.scss';
+
 import App from './components/app';
 
 ReactDOM.render(<App />, document.getElementById('root'));
