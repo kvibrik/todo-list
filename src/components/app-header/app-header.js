d3462ece966f3cc661980b0be72f3dacca491d82
kvibrik <kvibr@mail.ru>
2021-02-18 21:55:25 +0500

    change app-header

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -5,7 +5,9 @@ const AppHeader = () => {
   return (
     <div className="app-header">
       <h1 className="app-header__title">Todo List</h1>
-      <h2 className="app-header__subtitle">1 more to do, 2 done</h2>
+      <h2 className="app-header__subtitle">
+        {1} more to do, {2} done
+      </h2>
     </div>
   );
 };
