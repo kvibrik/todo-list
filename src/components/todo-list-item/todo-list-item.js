fc71f52a87b7487d40fed6dae55f09124297289e
kvibrik <kvibr@mail.ru>
2021-02-19 22:21:09 +0500

    styles for todo-list-item

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -16,10 +16,12 @@ export default class TodoListItem extends Component {
     return (
       <span className={classNames}>
         <span className="todo-list__label">{label}</span>
-        <button className="button button_non-padding" type="button">
+        <button className="button button_non-padding button_red" type="button">
           <i className="bi bi-trash" />
         </button>
-        <button className="button button_non-padding" type="button">
+        <button
+          className="button button_non-padding button_green"
+          type="button">
           <i class="bi bi-exclamation"></i>
         </button>
       </span>
